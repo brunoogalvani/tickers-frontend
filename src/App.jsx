ca8a9aad454d8ce6eaa9b4ba0647cfd59e8dca67
aqthurn <arthurnabas17@gmail.com>
2025-04-12 13:23:50 -0300

    inicio frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <header className="flex justify-evenly bg-red-900 text-white p-4">
+        
+        <h1>Tickers</h1>
+        <h2>Search</h2>
+      </header>
+      <h1 className="font-bold">Hello World!</h1>
+      
     </>
   )
 }
