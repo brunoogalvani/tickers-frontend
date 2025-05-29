bfb25eb0be3776916f07237a62b17c6f6e73c346
brunoogalvani <bthezolin@gmail.com>
2025-05-29 18:17:25 -0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register.jsx'
-import Promoter from './pages/promoter.jsx'
+import Promoter from './pages/Promoter.jsx'
 import PaginaUsuario from './pages/PaginaUsuario.jsx'
 function App() {
 
   return (
     <Router>
       <Routes>
-          <Route path='/PaginaUsuario' element={<PaginaUsuario />}></Route>
+          <Route path='/pagina-usuario' element={<PaginaUsuario />}></Route>
           <Route path='/register' element={<Register />}></Route>        
           <Route index element={<Home />}></Route>
           <Route path='/login' element={<Login />}></Route>
-          <Route path='/Promoter' element={<Promoter />}></Route>
+          <Route path='/promoter' element={<Promoter />}></Route>
       </Routes>
     </Router>
   )
