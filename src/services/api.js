4a9d9b054af4a7262fe5efa78b0eab5fffee456f
brunoogalvani <bthezolin@gmail.com>
2025-05-30 16:12:48 -0300

    fix urls to api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/",
+  baseURL: "https://tickers-backend.vercel.app/",
 });
 
 export default api;
