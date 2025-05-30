5d2e10785c79b1bc0b91dcaa4455c09165ffc3e5
brunoogalvani <bthezolin@gmail.com>
2025-05-30 16:05:39 -0300

    fix variavel de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-import dotenv from 'dotenv'
-dotenv.config()
 
 const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:8080/",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/",
 });
 
 export default api;
